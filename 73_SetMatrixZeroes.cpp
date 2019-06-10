bef361ef3d89aef3a3f0d858729374dda4e82b65
Son Dinh <technology789@gmail.com>
2019-06-10 13:38:25 -0500

    constant space

--- 73_SetMatrixZeroes.cpp
+++ 73_SetMatrixZeroes.cpp
@@ -1,5 +1,56 @@
 class Solution {
 public:
+    // Constant space.
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        bool firstCol = false;
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    if (j == 0) {
+                        firstCol = true;
+                    } else {
+                        matrix[i][0] = 0;
+                        matrix[0][j] = 0;
+                    }
+                }
+            }
+        }
+        
+        // Go through rows from 1.
+        for (int i = 1; i < m; ++i) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < n; ++j) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        // Go through columns from 1.
+        for (int j = 1; j < n; ++j) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < m; ++i) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        // Set zeroes for row 1.
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; ++j) {
+                matrix[0][j] = 0;
+            }
+        }
+        
+        // Set zeroes for column 1.
+        if (firstCol) {
+            for (int i = 0; i < m; ++i) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+	
     // O(m + n) space.
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();

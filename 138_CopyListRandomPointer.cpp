070d299a95db483cf3953938d8bcf813cfaa8517
Son Dinh <technology789@gmail.com>
2019-06-11 23:07:53 -0500

    O(1) space

--- 138_CopyListRandomPointer.cpp
+++ 138_CopyListRandomPointer.cpp
@@ -17,6 +17,47 @@ public:
 */
 class Solution {
 public:
+    // O(n) time, O(1) space.
+    Node* copyRandomList(Node *head) {
+        Node *curr = head;
+        Node *ret = nullptr;
+        
+        // Create clone nodes.
+        while (curr) {
+            Node *clone = new Node(curr->val, nullptr, nullptr);
+            if (!ret)
+                ret = clone;
+            
+            Node *next = curr->next;
+            curr->next = clone;
+            clone->next = next;
+            curr = next;
+        }
+        
+        curr = head;
+        // Set random pointers for the clone nodes.
+        while (curr) {
+            Node *clone = curr->next;
+            if (curr->random)
+                clone->random = curr->random->next;
+            
+            curr = curr->next->next;
+        }
+        
+        curr = head;
+        // Set the next pointers for the original and clone nodes.
+        while (curr) {
+            Node *clone = curr->next;
+            curr->next = curr->next->next;
+            curr = clone->next;
+            
+            if (clone->next)
+                clone->next = clone->next->next;
+        }
+        
+        return ret;
+    }
+	
     // O(n) space and time.
     Node* copyRandomList(Node* head) {
         unordered_map<Node*, Node*> old_to_new;

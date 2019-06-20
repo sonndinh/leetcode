5ffd1b7996d49a7f8febf78636edf409852590b2
Son Dinh <technology789@gmail.com>
2019-06-19 23:39:24 -0500

    compute block-based maximum

--- 239_SlidingWindowMaximum.cpp
+++ 239_SlidingWindowMaximum.cpp
@@ -16,32 +16,26 @@ public:
         return ret;
     }
     
+    // Using the 3rd idea in the solution.
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> ret;
-        if (nums.empty()) 
-            return ret;
-        if (k == 1)
+        if (k == 1 || k == 0)
             return nums;
         
-        int max_all = nums[0];
-        int max_trail = k == 2 ? 0 : numeric_limits<int>::min();
-        for (int i = 0; i < k; ++i) {
-            if (nums[i] > max_all) {
-                max_all = nums[i];
-            }
-            if (i > 1 && nums[i] > max_trail) {
-                max_trail = nums[i];
-            }
+        vector<int> ret;
+        int n = nums.size();
+        vector<int> left = nums, right = nums;
+        
+        for (int i = 0; i < n; ++i) {
+            left[i] = i % k > 0 ? max(left[i], left[i-1]) : left[i];
+            int j = n - 1 - i;
+            right[j] = (j+1) % k > 0 ? (j == n-1 ? right[j] : max(right[j], right[j+1])) : right[j];
         }
         
-        ret.push_back(max_all);
-        
-        int n = nums.size();
-        for (int i = 1; i <= n-k; ++i ) {
-            int last_entry = nums[i + k - 1];
-            max_all = max(nums[i], max(max_trail, last_entry));
-            max_trail = (k == 2 ? 0 : max(max_trail, last_entry));
-            ret.push_back(max_all);
+        for (int i = 0; i <= n-k; ++i) {
+            if (i % k > 0) 
+                ret.push_back(max(right[i], left[i + k - 1]));
+            else
+                ret.push_back(left[i + k - 1]);
         }
         
         return ret;

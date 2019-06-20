29a89b58d50369b84bfe2fff7f600fd3de8548fd
Son Dinh <technology789@gmail.com>
2019-06-20 16:43:05 -0500

    cleaner

--- 124_BinaryTreeMaximumPathSum.cpp
+++ 124_BinaryTreeMaximumPathSum.cpp
@@ -11,7 +11,7 @@ class Solution {
 public:
     // Return 2 values: maximum sum of this subtree from path that may or may not
     // go through this node AND maximum sum of path ending at this node.
-    vector<int> visit(TreeNode* node) {
+    pair<int, int> visit(TreeNode* node) {
         if (!node)
             return {numeric_limits<int>::min(), numeric_limits<int>::min()};
         
@@ -19,25 +19,17 @@ public:
         auto right = visit(node->right);
         
         // Path with maximum sum from the two subtrees.
-        int max_children = max(left[0], right[0]);
+        int max_children = max(left.first, right.first);
         
         // Maximum sum of path going through this node.
-        int max_through_node = node->val;
-        int max_end_at_node = node->val;
-        if (left[1] > 0) {
-            max_through_node += left[1];
-            max_end_at_node += left[1];
-        }
-        if (right[1] > 0) {
-            max_through_node += right[1];
-            max_end_at_node = max(max_end_at_node, node->val + right[1]);
-        }
+        int max_through_node = node->val + max(left.second, 0) + max(right.second, 0);
+        int max_end_at_node = node->val + max(max(left.second, 0), max(right.second, 0));
         
         return {max(max_children, max_through_node), max_end_at_node};
     }
     
     int maxPathSum(TreeNode* root) {
-        vector<int> ret = visit(root);
-        return ret[0];
+        pair<int, int> ret = visit(root);
+        return ret.first;
     }
 };

68a7c99ab919810c60bdb18add78643e22846bcd
Son Dinh <technology789@gmail.com>
2019-07-03 18:10:16 -0500

    use A* search

--- 675_CutOffTreesForGolfEvent.cpp
+++ 675_CutOffTreesForGolfEvent.cpp
@@ -13,7 +13,6 @@ public:
         visited.insert(i * n + j);
         
         vector<pair<int,int>> adj{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int dist = 0;
         while (!frontier.empty()) {
             auto cell = frontier.front();
             frontier.pop();
@@ -34,6 +33,7 @@ public:
         return {-1, -1, -1};
     }
     
+    // Use normal BFS.
     int cutOffTree1(vector<vector<int>>& forest) {
         if (forest.empty()) return 0;
         if (forest[0][0] == 0) return -1;
@@ -128,13 +128,10 @@ public:
                 dist = min(dist, p.second + visited_t[p.first]);
             }
         }
-        
         return dist;
     }
     
-    int cutOffTree(vector<vector<int>>& forest) {
-        if (forest.empty()) return 0;
-
+    vector<tuple<int,int,int>> get_heights(const vector<vector<int>>& forest) {
         int m = forest.size(), n = forest[0].size();
         vector<tuple<int, int, int>> heights;
         for (int i = 0; i < m; ++i) {
@@ -147,19 +144,69 @@ public:
         if (forest[0][0] == 0 || forest[0][0] == 1)
             heights.push_back({0, 0, forest[0][0]});
         sort(heights.begin(), heights.end(), Compare());
+        return heights;
+    }
+    
+    struct AstarCompare {
+        bool operator() (const vector<int>& a, const vector<int>& b) const {
+            return a[2] > b[2]; // Compare total cost f = g + h.
+        }
+    };
+    
+    int astar(const vector<vector<int>>& board, const tuple<int,int,int>& s, const tuple<int,int,int>& t) {
+        int m = board.size(), n = board[0].size();
+        priority_queue<vector<int>, vector<vector<int>>, AstarCompare> min_heap;
+        unordered_map<int, int> node_to_cost; // Map from tree to total cost (not real cost).
+        min_heap.push({get<0>(s), get<1>(s), 0, 0});
+        node_to_cost.insert({get<0>(s) * n + get<1>(s), 0});
+        
+        vector<pair<int, int>> adj{{-1,0}, {1,0}, {0,-1}, {0,1}};
+        while (!min_heap.empty()) {
+            auto cell = min_heap.top();
+            min_heap.pop();
+            if (cell[0] == get<0>(t) && cell[1] == get<1>(t)) {
+                // Found the target.
+                return cell[3];
+            }
+            
+            for (auto p : adj) {
+                int x = cell[0] + p.first, y = cell[1] + p.second;
+                if (x >= 0 && x < m && y >= 0 && y < n && board[x][y] != 0) {
+                    // Relax distance to (x,y): f(x,y) = g(x,y) + h(x,y).
+                    int g_dist = cell[3] + 1;
+                    int h_dist = abs(x - get<0>(t)) + abs(y - get<1>(t));
+                    if (node_to_cost.find(x * n + y) == node_to_cost.cend() || 
+                       g_dist + h_dist < node_to_cost[x * n + y]) {
+                        min_heap.push({x, y, g_dist + h_dist, g_dist});
+                        node_to_cost[x*n + y] = g_dist + h_dist;
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
+    
+    int cutOffTree(vector<vector<int>>& forest) {
+        if (forest.empty()) return 0;
+
+        // Each tuple contains coordinate (x,y) and corresponding heights.
+        vector<tuple<int, int, int>> heights = get_heights(forest);
         
         // May need to walk from (0, 0) to the cell with shortest tree first
         // since we always cut trees in increasing heights.
         int dist = 0;
         if (forest[0][0] > get<2>(heights[0])) {
-            int ret = bidirect_bfs(forest, {0, 0, forest[0][0]}, heights[0]);
+            //int ret = bidirect_bfs(forest, {0, 0, forest[0][0]}, heights[0]);
+            int ret = astar(forest, {0, 0, forest[0][0]}, heights[0]);
             if (ret == -1)
                 return -1;
             dist += ret;
         }
             
         for (int i = 0; i < heights.size()-1; ++i) {
-            int ret = bidirect_bfs(forest, heights[i], heights[i+1]);
+            //int ret = bidirect_bfs(forest, heights[i], heights[i+1]);
+            int ret = astar(forest, heights[i], heights[i+1]);
             if (ret == -1)
                 return -1;
             dist += ret;

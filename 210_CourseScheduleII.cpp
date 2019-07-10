ea223722fe18b2abf3436758cb2ec0dce659eae6
Son Dinh <technology789@gmail.com>
2019-07-09 23:56:13 -0500

    faster

--- 210_CourseScheduleII.cpp
+++ 210_CourseScheduleII.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    bool acyclic(int i, vector<vector<int>>& adj_list, vector<int>& courses, vector<int>& visited) {
+        if (visited[i] == 2)
+            return false;
+        if (visited[i] == 1)
+            return true;
+        
+        visited[i] = 2;
+        for (int adj : adj_list[i]) {
+            if (!acyclic(adj, adj_list, courses, visited))
+                return false;
+        }
+        
+        visited[i] = 1;
+        courses.push_back(i);
+        return true;
+    }
+    
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj_list(numCourses);
+        for (auto& vec : prerequisites) {
+            adj_list[vec[1]].push_back(vec[0]);
+        }
+        
+        vector<int> visited(numCourses, 0);
+        vector<int> courses;
+        for (int i = 0; i < numCourses; ++i) {
+            if (visited[i] == 0 && !acyclic(i, adj_list, courses, visited))
+                return vector<int>();
+        }
+     
+        reverse(courses.begin(), courses.end());
+        return courses;
+    }
+	
     bool is_cyclic(int id, const vector<vector<int>>& adj_list, unordered_set<int>& traversed, unordered_set<int>& visited) {
         if (visited.find(id) != visited.cend()) 
             return false;
@@ -23,7 +57,7 @@ public:
         courses.push_front(i);
     }
     
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> findOrder1(int numCourses, vector<vector<int>>& prerequisites) {
         // Construct adjacency list for the graph.
         vector<vector<int>> adj_list(numCourses);
         for (auto& vec : prerequisites) {

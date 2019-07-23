86ca6915a2049ff07efcd02d1cdce078c7bb1a45
Son Dinh <technology789@gmail.com>
2019-07-23 10:12:33 -0500

    one pass

--- 75_SortColors.cpp
+++ 75_SortColors.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        int i = -1, k = nums.size(), j = 0;
+        
+        while (j < k) {
+            if (nums[j] == 0) {
+                swap(nums[i+1], nums[j]);
+                ++i;
+                ++j;
+            } else if (nums[j] == 2) {
+                swap(nums[k-1], nums[j]);
+                --k;
+            } else {
+                ++j;
+            }
+        }
+    }
+    
+    void sortColors1(vector<int>& nums) {
         int count_zero = 0, count_one = 0, count_two = 0;
         for (int num : nums) {
             if (num == 0)

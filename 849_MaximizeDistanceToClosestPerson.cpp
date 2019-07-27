61e7aaa784afd5527a8022ce4c292bdb12289c4b
Son Dinh <technology789@gmail.com>
2019-07-27 00:44:06 -0500

    constant space

--- 849_MaximizeDistanceToClosestPerson.cpp
+++ 849_MaximizeDistanceToClosestPerson.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
-    // Time O(n), space O(n).
+    // Time O(n), space O(1).
     int maxDistToClosest(vector<int>& seats) {
+        int left = -1, right = 0;
+        int max_dist = 0;
+        for (int i = 0; i < seats.size(); ++i) {
+            if (seats[i] == 1) {
+                left = i;
+            } else {
+                if (i >= right) {
+                    right = i;
+                    while (right < seats.size() && seats[right] == 0)
+                        ++right;
+                }
+                if (i == 0)
+                    max_dist = right - i;
+                else
+                    max_dist = right == seats.size()? max(max_dist, i - left) : max(max_dist, min(i - left, right - i));
+            }
+        }
+        
+        return max_dist;
+    }
+	
+    // Time O(n), space O(n).
+    int maxDistToClosest1(vector<int>& seats) {
         int n = seats.size();
         vector<int> dist(n);
         
